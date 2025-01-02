2680565643d0a2e85d2a7564b6eedaa8c5d1d215
Guest-User-1 <shreesevaumbarli@gmail.com>
2025-01-02 21:52:31 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    open: true,
-    historyApiFallback: true,
-  },
-  build: {
-    outDir: "dist",
-  },
-  // Add this for client-side routing
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
-  },
 });
