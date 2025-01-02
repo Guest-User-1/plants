3b8912965613719ecb70b7d880ba53cc139cfb00
Guest-User-1 <shreesevaumbarli@gmail.com>
2025-01-03 00:15:15 +0530

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -3,6 +3,7 @@ self.addEventListener('install', (event) => {
     caches.open('v1').then((cache) => {
       return cache.addAll([
         '/',
+        '/login',
         '/index.html',
         '/manifest.json'
       ]);
