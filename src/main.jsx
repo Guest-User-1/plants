9954773a09f48818c3787ac9ba043b361125fcb0
Guest-User-1 <shreesevaumbarli@gmail.com>
2025-01-02 23:37:05 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,12 @@ import { AuthProvider } from "./context/AuthContext.jsx";
 import "./index.css";
 import App from "./App.jsx";
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js");
+  });
+}
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
